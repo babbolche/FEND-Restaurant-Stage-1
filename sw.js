96ef5890db67f59ebb446f8eb339922a5b0a77b7
Marina Radeva <marina_radeva@yahoo.com>
2018-07-18 21:45:40 +0300

    Fix service worker cache issue

--- sw.js
+++ sw.js
@@ -1,49 +1,31 @@
-var CACHE_NAME = 'app-static-v1-cache';
-
-var urlsToCache = [
-    '/',
-    'css/styles.css',
-    'data/restaurants.json',
-    'sw-reg.js',
-    'sw.js',
-    'js/dbhelper.js',
-    'js/main.js',
-    'js/restaurant_info.js',
-    'index.html',
-    'restaurant.html',
-    'img/1.jpg',
-    'img/2.jpg',
-    'img/3.jpg',
-    'img/4.jpg',
-    'img/5.jpg',
-    'img/6.jpg',
-    'img/7.jpg',
-    'img/8.jpg',
-    'img/9.jpg',
-    'img/10.jpg'
-];
-
-self.addEventListener('install', function(event) {
-    // Perform install steps
+self.addEventListener('install', event => {
     event.waitUntil(
-        caches.open(CACHE_NAME)
-        .then(function(cache) {
-            console.log('Opened cache');
-            return cache.addAll(urlsToCache);
-        })
-    );
+        caches.open('fend-restaurant-stage-1').then(cache => {
+            return cache.addAll([
+                '/',
+                '/css/styles.css',
+                '/js/main.js',
+                '/js/dbhelper.js',
+                '/data/restaurants.json',
+                'index.html',
+                'restaurant.html',
+                'img/1.jpg',
+                'img/2.jpg',
+                'img/3.jpg',
+                'img/4.jpg',
+                'img/5.jpg',
+                'img/6.jpg',
+                'img/7.jpg',
+                'img/8.jpg',
+                'img/9.jpg',
+                'img/10.jpg'
+                ]);
+        }));
 });
 
-self.addEventListener('fetch', function(event) {
+self.addEventListener('fetch', event => {
     event.respondWith(
-        caches.match(event.request)
-        .then(function(response) {
-            // Cache hit - return response
-            if (response) {
-            return response;
-            }
-            return fetch(event.request);
-        }
-        )
-    );
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        }));
 });
